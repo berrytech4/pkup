4b111b9eab9248f363f146bec92f98e0976eea3e
berrytech4 <140203814+berrytech4@users.noreply.github.com>
2026-02-13 10:39:17 +0000

    uudate

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function sendPickup() {
-  const data = {
-    parent_name: document.getElementById("parent").value,
-    child_name: document.getElementById("child").value,
-    class: document.getElementById("class").value,
-    teacher: document.getElementById("teacher").value,
-  };
 
-  fetch(
-    "https://script.google.com/macros/s/AKfycbwfSLe8k6SSOStmvrlY7zD_pDkmWQYbAqrQifWcA-UCDPS6ueXAj5VdeHeaK2nR-gHXKA/exec",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    },
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      document.getElementById("message").innerText = "Notification Sent!";
-    })
-    .catch((error) => {
-      document.getElementById("message").innerText = "Error sending request";
-      console.log(error);
-    });
+  const parent = document.getElementById("parent").value;
+  const child = document.getElementById("child").value;
+  const className = document.getElementById("class").value;
+  const teacher = document.getElementById("teacher").value;
+
+  const formData = new URLSearchParams();
+  formData.append("parent_name", parent);
+  formData.append("child_name", child);
+  formData.append("class", className);
+  formData.append("teacher", teacher);
+
+  fetch("https://script.google.com/macros/s/AKfycbwfSLe8k6SSOStmvrlY7zD_pDkmWQYbAqrQifWcA-UCDPS6ueXAj5VdeHeaK2nR-gHXKA/exec", {
+    method: "POST",
+    body: formData
+  })
+  .then(response => response.text())
+  .then(result => {
+    document.getElementById("message").innerText = "Notification Sent!";
+  })
+  .catch(error => {
+    document.getElementById("message").innerText = "Error sending request";
+    console.log(error);
+  });
+
 }
