4792a899641da7525ae0e6c12f4d9331159537a1
berrytech4 <140203814+berrytech4@users.noreply.github.com>
2026-02-13 14:44:48 +0000

    update

--- script.js
+++ script.js
@@ -5,24 +5,19 @@ function sendPickup() {
   const className = document.getElementById("class").value;
   const teacher = document.getElementById("teacher").value;
 
-  const formData = new URLSearchParams();
+  const url = "https://script.google.com/macros/s/AKfycbw3UYTFTOF97b0AjRoxGoI06lhryWGrMMGxK8K-FCy7PyMdlxYjojsKZM8zFsKVxef5/exec";
+
+  const formData = new FormData();
   formData.append("parent_name", parent);
   formData.append("child_name", child);
   formData.append("class", className);
   formData.append("teacher", teacher);
 
-  fetch("https://script.google.com/macros/s/AKfycbw3UYTFTOF97b0AjRoxGoI06lhryWGrMMGxK8K-FCy7PyMdlxYjojsKZM8zFsKVxef5/exec", {
+  fetch(url, {
     method: "POST",
-    body: formData
-  })
-  .then(response => response.text())
-  .then(result => {
-    document.getElementById("message").innerText = "Notification Sent!";
-  })
-  .catch(error => {
-    document.getElementById("message").innerText = "Error sending request";
-    console.log(error);
+    body: formData,
+    mode: "no-cors"
   });
 
+  document.getElementById("message").innerText = "Notification Sent!";
 }
-
